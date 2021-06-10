00479fef70970847a3ad6479d611e7094560d1b9
Khadija Colibaly <Kcoulibaly360@gmail.com>
2021-06-09 21:10:13 -0400

    Added main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+enum eDirection { STOP=0, LEFT=1, UPLEFT=2, DOWNLEFT=3, RIGHT=4, UPRIGHT=5, DOWN=6};
+class cBall{
+    private:
+    int x;
+    int y;
+    int originX, originY;
+};
+int main(){
+    return 0;
+}
